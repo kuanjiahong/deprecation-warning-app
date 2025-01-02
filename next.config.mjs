8551438978c0e2b4e2bc4855815796b9a5e8ff5e
Kuan Jia Hong <jia-hong.kuan@polyu.edu.hk>
2025-01-02 11:02:23 +0800

    Add rewrites function

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@
  */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/some-rewrite-url",
+        destination: "http://localhost:3000/url-rewritten"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
