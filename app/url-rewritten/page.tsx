ad8c15639ec92fc62485452850646fa9cb98b3cc
Kuan Jia Hong <jia-hong.kuan@polyu.edu.hk>
2025-01-02 11:03:20 +0800

    Add page for rewritten url

---
+++ app/url-rewritten/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Page() {
+    return (
+        <div>
+            <p>The url rewritten</p>
+            <p>Check the console for any deprecation warning</p>
+        </div>
+    )
+}
