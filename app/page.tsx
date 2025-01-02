4dd7e8460cda1a62558ae64128276a442ce5f640
Kuan Jia Hong <jia-hong.kuan@polyu.edu.hk>
2025-01-02 11:03:07 +0800

    Add button to rewrite url

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null;
+  return (
+    <div>
+      <p>Click the button to go to /some-rewrite-url</p>
+      <p>The app will rewrite the url to /url-written</p>
+      <button>
+        <a href="/some-rewrite-url">Click me to trigger the rewrite feature</a>
+      </button>
+    </div>
+  );
 }
